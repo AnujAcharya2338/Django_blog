22a496aa4a7e7e6cee72dab1086e7a52d9ca46dd
AnujAcharya2338 <anujacharya877@gmail.com>
2026-01-05 18:56:07 +0545

    Datas fetched from the database

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blogs.models import Blog, Category
+
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured = True, status = 'Published').order_by('-updated_at')
+    post = Blog.objects.filter(is_featured = False, status = 'Published').order_by('-updated_at')
+    contex = {
+        'categories':categories,
+        'featured_posts': featured_post,
+        'posts': post,  
+    }
+    return render(request, 'home.html', contex)  
+    
---
+++ blogs/urls.py
@@ -0,0 +1,6 @@
+from . import views
+from django.urls import include, path
+ 
+urlpatterns = [
+    path('<int:category_id>/', views.post_by_category, name='post_by_category'),
+] 
