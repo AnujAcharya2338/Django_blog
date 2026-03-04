0a290b20ffa9729d74035239c9ff7c0710501453
AnujAcharya2338 <anujacharya877@gmail.com>
2026-03-04 20:54:21 +0545

    created 2 new template

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*%07w6myjxwhdw)6nb#_9soglazhgj9eb!59+fr4)gd#fkw(t^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from blogs.models import Blog
+from blogs.models import Blog, Category 
 
 # Create your views here.
+from django.shortcuts import get_object_or_404
+
 def post_by_category(request, category_id): 
-    posts = Blog.objects.filter(status = 'Published', category = category_id)
-    print(posts)
-    return HttpResponse(posts)         
+
+    try:
+        category_id = int(category_id)
+    except ValueError:
+        return redirect('home')
+
+    # Use get_object_or_404 to handle missing categories safely
+    category = get_object_or_404(Category, pk=category_id)
+
+    posts = Blog.objects.filter(status='Published', category=category)
+    
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'post_by_category.html', context)
