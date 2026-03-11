9e76c7f40eda54dfddf6b45b6396975a03f74de2
AnujAcharya2338 <anujacharya877@gmail.com>
2026-03-11 16:03:04 +0545

    About section and social link

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,15 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from assignments.models import About
 from blogs.models import Blog, Category
 
 
 def home(request):
     featured_post = Blog.objects.filter(is_featured = True, status = 'Published').order_by('-updated_at')
     post = Blog.objects.filter(is_featured = False, status = 'Published').order_by('-updated_at')
+
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
     contex = {
         'featured_posts': featured_post,
         'posts': post,  
+        'about': about,  
     }
     return render(request, 'home.html', contex)  
     
+
