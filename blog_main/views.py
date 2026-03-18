56ca928411cdbc45bd100caf9243b9d128124a06
AnujAcharya2338 <anujacharya877@gmail.com>
2026-03-18 12:27:12 +0545

    edit_button and delete function (crud)

--- blog_main/views.py
+++ blog_main/views.py
@@ -49,11 +49,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                return redirect('/')
-
-
-
-
+                return redirect('dashboard')
     form = AuthenticationForm()
     context  = {
         'form': form,
