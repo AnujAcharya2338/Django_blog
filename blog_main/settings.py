fe9d9133a40ab748bc4d5062cc89c5adc38b2af5
AnujAcharya2338 <anujacharya877@gmail.com>
2026-04-02 22:37:44 +0545

    fix templates dir case

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'blog_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
